061694f5aece627d1c45c809f8f8433e17707723
jincheol5 <oscar4293@gmail.com>
2026-03-10 01:44:24 +0900

    update

--- test/test.py
+++ test/test.py
@@ -1,8 +1,20 @@
 import requests
 
-url=f"https://blockchain.info/q/getblockcount"
+# url=f"https://blockchain.info/q/getblockcount"
 
-response=requests.get(url)
+# response=requests.get(url)
 
+# print(response.status_code)
+# print(int(response.text))
+
+
+block_height=0
+url=f"https://blockchain.info/block-height/{block_height}"
+params={
+    "format":"json"
+}
+response=requests.get(url,params=params)
 print(response.status_code)
-print(int(response.text))
+print(response.text)
+
+# genesis hash=000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f
